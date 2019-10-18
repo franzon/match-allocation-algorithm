fece700e39842b109232729f45e483d7c2e738ad
Jorge Rossi <jorgefzrossi@gmail.com>
2019-10-18 09:51:21 -0300

    Reestruturando o código

---
+++ allocator.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/MaxHalford/eaopt"
+)
+
+// ResponseBody Representação da resposta
+type ResponseBody struct {
+	NFullSlots  int    `json:"n_full_slots"`
+	NHalfSlots  int    `json:"n_half_slots"`
+	NEmptySlots int    `json:"n_empty_slots"`
+	Slots       []Slot `json:"slots"`
+}
+
+type Slot struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	Match       Match  `json:"match"`
+	Status      string `json:"status"`
+}
+
+type Player struct {
+	ID        int      `json:"id"`
+	Name      string   `json:"name"`
+	AbleSlots []string `json:"able_slots"`
+	Status    bool     `json:"status"`
+}
+
+type Match struct {
+	ID      int
+	Player1 Player `json:"player1"`
+	Player2 Player `json:"player2"`
+}
+
+// ScheduleData Dados necessários para calcular o fitness
+type ScheduleData struct {
+	Slots   []Slot
+	Players []Player
+	Matchs  []Match
+}
+
+// Schedule Representação do cromossomo
+type Schedule struct {
+	Genome []int
+	Data   ScheduleData
+}
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+func FindPlayerByID(id int, players []Player) (Player, error) {
+	for _, p := range players {
+		if p.ID == id {
+			return p, nil
+		}
+	}
+	return Player{}, fmt.Errorf("Couldn't found Player")
+}
+
+func FindMatchByID(id int, matchs []Match) (Match, error) {
+	for _, p := range matchs {
+		if p.ID == id {
+			return p, nil
+		}
+	}
+	return Match{}, fmt.Errorf("Couldn't found Match")
+}
+
+func (X Schedule) BuildResponseFromSchedule() ResponseBody {
+
+	body := ResponseBody{
+		Slots:      make([]Slot, len(X.Data.Slots)),
+		NFullSlots: 0,
+		NHalfSlots: 0,
+	}
+
+	copy(body.Slots, X.Data.Slots)
+
+	// Número de condições satisfeitas
+	count := 0
+
+	for slotIndex, matchID := range X.Genome {
+
+		slot := X.Data.Slots[slotIndex]
+
+		// Pula os slots vazios
+		if matchID != -1 {
+			match, _ := FindMatchByID(matchID, X.Data.Matchs)
+
+			body.Slots[slotIndex].Match = match
+
+			tmpCount := count
+
+			if Contains(match.Player1.AbleSlots, slot.ID) && Contains(match.Player2.AbleSlots, slot.ID) {
+
+				body.Slots[slotIndex].Status = "full"
+				body.NFullSlots++
+				count += 2
+
+			} else {
+
+				if Contains(match.Player1.AbleSlots, slot.ID) {
+					body.Slots[slotIndex].Status = "half"
+					body.NHalfSlots++
+					count++
+
+					body.Slots[slotIndex].Match.Player1.Status = true
+				}
+
+				if Contains(match.Player2.AbleSlots, slot.ID) {
+					body.Slots[slotIndex].Status = "half"
+					body.NHalfSlots++
+					count++
+
+					body.Slots[slotIndex].Match.Player2.Status = true
+				}
+			}
+
+			if count == tmpCount {
+				body.Slots[slotIndex].Status = "empty"
+
+				body.NEmptySlots++
+
+			}
+
+		}
+	}
+
+	return body
+}
+
+// Evaluate Calcula o fitness de um cromossomo
+// Quanto menor o valor, melhor o cromossomo resolve o problema
+func (X Schedule) Evaluate() (float64, error) {
+
+	// Descartando soluções inválidas
+
+	withoutEmpty := make([]int, 0)
+
+	for index := 0; index < len(X.Genome); index++ {
+		if X.Genome[index] != -1 {
+			withoutEmpty = append(withoutEmpty, X.Genome[index])
+		}
+	}
+
+	// Adiciona em um map para remover partidas que aparecem mais de uma vez
+	slotMap := make(map[int]bool)
+	for index := 0; index < len(withoutEmpty); index++ {
+		slotMap[withoutEmpty[index]] = true
+	}
+
+	// Retorna um fitness grande para descartar cromossomos onde a partida aparece mais de uma vez
+	// ou não aparecem todas as partidas
+	if (len(slotMap) != len(withoutEmpty)) || (len(slotMap) != len(X.Data.Matchs)) {
+		return math.MaxFloat64, nil
+	}
+
+	response := X.BuildResponseFromSchedule()
+
+	return -float64(response.NFullSlots*10 + response.NHalfSlots), nil
+}
+
+// Mutate Aplica a mutação
+func (X Schedule) Mutate(rng *rand.Rand) {
+	eaopt.MutPermuteInt(X.Genome, 1, rng)
+}
+
+// Crossover Aplica o cruzamento
+func (X Schedule) Crossover(Y eaopt.Genome, rng *rand.Rand) {
+	eaopt.CrossGNXInt(X.Genome, Y.(Schedule).Genome, 1, rng)
+}
+
+// Clone Clona um genoma
+func (X Schedule) Clone() eaopt.Genome {
+	var Y = Schedule{Genome: make([]int, len(X.Genome)), Data: X.Data}
+	copy(Y.Genome, X.Genome)
+	return Y
+}
+
+// func RemoveIndex(s []Player, index int) []Player {
+// 	return append(s[:index], s[index+1:]...)
+// }
+
+// GenomeFactory Cria soluções aleatórias
+func (data ScheduleData) GenomeFactory(rng *rand.Rand) eaopt.Genome {
+
+	// tmpPlayers := make([]Player, len(data.Players))
+	// copy(tmpPlayers, X.Players)
+
+	rndGenome := make([]int, len(data.Slots))
+
+	// Preenche todos os slots como vazios
+	for index := 0; index < len(data.Slots); index++ {
+		rndGenome[index] = -1
+	}
+
+	// Adiciona as partidas nos slots
+	for index := 0; index < len(data.Matchs); index++ {
+		rndGenome[index] = data.Matchs[index].ID
+	}
+
+	// Embaralha o genoma
+	rand.Shuffle(len(rndGenome), func(i, j int) {
+		rndGenome[i], rndGenome[j] = rndGenome[j], rndGenome[i]
+	})
+
+	return Schedule{Genome: rndGenome, Data: data}
+}
--- main.go
+++ main.go
@@ -11,242 +11,141 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type AllocateRequestBody struct {
-	Slots   []Slot   `json:"slots"`
-	Players []Player `json:"players"`
-	Matchs  []Match  `json:"matchs"`
-}
+// -------------------- Request --------------------
 
-type AllocateRequestResponse struct {
-	Slots   []int   `json:"slots"`
-	Fitness float64 `json:"fitness"`
-}
+// // RequestSlot Representação do Slot na requisição
+// type RequestSlot struct {
+// 	ID          int    `json:"id"`
+// 	Description string `json:"description"`
+// }
 
-type Slot struct {
-	ID          string `json:"id"`
-	Description string `json:"description"`
-}
+// // RequestPlayer Representação do Player na requisição
+// type RequestPlayer struct {
+// 	ID           int      `json:"id"`
+// 	Name         string   `json:"name"`
+// 	AbleSlotsIDs []string `json:"able_slots"`
+// }
 
-type Player struct {
-	ID        int      `json:"id"`
-	Name      string   `json:"name"`
-	AbleSlots []string `json:"able_slots"`
-}
-
-type Match struct {
+// RequestMatch Representação da Match na requisição
+type RequestMatch struct {
 	ID      int `json:"id"`
 	Player1 int `json:"player1"`
 	Player2 int `json:"player2"`
 }
 
-type Schedule struct {
-	Genome  []int
-	Slots   []Slot
-	Players []Player
-	Matchs  []Match
+// RequestBody Representação da requisição
+type RequestBody struct {
+	Slots   []Slot         `json:"slots"`
+	Players []Player       `json:"players"`
+	Matchs  []RequestMatch `json:"matchs"`
 }
 
-func Contains(a []string, x string) bool {
-	for _, n := range a {
-		if x == n {
-			return true
-		}
-	}
-	return false
-}
+// -------------------- Response --------------------
 
-func FindPlayerByID(id int, players []Player) (Player, error) {
-	for _, p := range players {
-		if p.ID == id {
-			return p, nil
-		}
-	}
-	return Player{}, fmt.Errorf("Couldn't found Player")
-}
+// InitGeneticAlgorithm Inicializa o algoritmo genético
+func RunGeneticAlgorithm(body RequestBody) (ResponseBody, error) {
 
-func (X Schedule) Evaluate() (float64, error) {
-
-	fitness := 1.0
-
-	count := 0
-
-	for index := 0; index < len(X.Genome); index++ {
-
-		matchID := X.Genome[index]
-		slot := X.Slots[index]
-
-		if matchID != -1 {
-
-			for _, match := range X.Matchs {
-				if match.ID == matchID {
-
-					player1, _ := FindPlayerByID(match.Player1, X.Players)
-					player2, _ := FindPlayerByID(match.Player2, X.Players)
-
-					if Contains(player1.AbleSlots, slot.ID) && Contains(player2.AbleSlots, slot.ID) {
-						count++
-					}
-
-					break
-
-					// if Contains(player1.AbleSlots, slot.ID) {
-					// 	count++
-					// }
-
-					// if Contains(player2.AbleSlots, slot.ID) {
-					// 	count++
-					// }
-
-				}
-			}
-		}
-	}
-
-	withoutEmpty := make([]int, 0)
-
-	for index := 0; index < len(X.Genome); index++ {
-		if X.Genome[index] != -1 {
-			withoutEmpty = append(withoutEmpty, X.Genome[index])
-		}
-	}
-
-	slotMap := make(map[int]bool)
-	for index := 0; index < len(withoutEmpty); index++ {
-		slotMap[withoutEmpty[index]] = true
-	}
-
-	if len(slotMap) != len(withoutEmpty) {
-		// fmt.Println("Duplic")
-
-	} else if len(slotMap) != len(X.Matchs) {
-		// fmt.Println("faltant")
-	} else {
-
-		if count == len(X.Matchs) {
-			// if count == len(X.Matchs)*2 {
-			// fmt.Println("Solução ", X)
-			fitness = 0.0
-
-		} else if count == 0 {
-			fitness = 1.0
-		} else {
-			fitness = -float64(count) / float64(len(X.Matchs))
-			// fitness = float64(count) / float64(len(X.Matchs)*2)
-		}
-
-	}
-
-	return fitness, nil
-}
-
-func (X Schedule) Mutate(rng *rand.Rand) {
-	eaopt.MutPermuteInt(X.Genome, 1, rng)
-}
-
-func (X Schedule) Crossover(Y eaopt.Genome, rng *rand.Rand) {
-	eaopt.CrossGNXInt(X.Genome, Y.(Schedule).Genome, 1, rng)
-}
-
-func (X Schedule) Clone() eaopt.Genome {
-	var Y = Schedule{Genome: make([]int, len(X.Genome)), Slots: X.Slots, Players: X.Players, Matchs: X.Matchs}
-	copy(Y.Genome, X.Genome)
-	return Y
-}
-
-func RemoveIndex(s []Player, index int) []Player {
-	return append(s[:index], s[index+1:]...)
-}
-
-func (X Schedule) GenomeFactory(rng *rand.Rand) eaopt.Genome {
-
-	tmpPlayers := make([]Player, len(X.Players))
-	copy(tmpPlayers, X.Players)
-
-	rndSlots := make([]int, len(X.Slots))
-
-	for index := 0; index < len(X.Slots); index++ {
-		rndSlots[index] = -1
-	}
-
-	for index := 0; index < len(X.Matchs); index++ {
-		rndSlots[index] = X.Matchs[index].ID
-	}
-
-	rand.Shuffle(len(rndSlots), func(i, j int) {
-		rndSlots[i], rndSlots[j] = rndSlots[j], rndSlots[i]
-	})
-
-	return Schedule{Genome: rndSlots, Slots: X.Slots, Players: X.Players, Matchs: X.Matchs}
-}
-
-func GenerateAllocation(w http.ResponseWriter, r *http.Request) {
-
-	var body AllocateRequestBody
-	_ = json.NewDecoder(r.Body).Decode(&body)
-
-	first := time.Now()
-
+	// Função de parada do algorítmo genético
 	earlyStop := func(ga *eaopt.GA) bool {
-		// fmt.Println(ga.HallOfFame[0].Fitness)
-		return ga.HallOfFame[0].Fitness == 0.0
+		return ga.HallOfFame[0].Fitness == -float64(len(body.Matchs)*2)
 	}
 
-	gaConfig := eaopt.GAConfig{EarlyStop: earlyStop, NPops: 1, PopSize: 80, HofSize: 1, NGenerations: 300, ParallelEval: false, Model: eaopt.ModGenerational{
-		Selector: eaopt.SelTournament{
-			NContestants: 3,
-		},
-		MutRate:   0.5,
-		CrossRate: 0.7,
-	}}
+	// Configuração do algorítmo genético
+	gaConfig := eaopt.GAConfig{
+		EarlyStop: earlyStop, NPops: 100, PopSize: 80, HofSize: 1, NGenerations: 300, ParallelEval: true, Model: eaopt.ModGenerational{
+			Selector: eaopt.SelTournament{
+				NContestants: 3,
+			},
+			MutRate:   0.5,
+			CrossRate: 0.7,
+		}}
+
 	var ga, err = gaConfig.NewGA()
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		return ResponseBody{}, fmt.Errorf("erro")
 	}
 
-	// ga.Callback = func(ga *eaopt.GA) {
-	// 	// fmt.Printf("Best fitness at generation %d: %f\n", ga.Generations, ga.HallOfFame[0].Fitness)
-	// }
+	// Dados necessários para calcular o fitness do cromossomo
 
-	data := Schedule{Slots: body.Slots, Matchs: body.Matchs, Players: body.Players}
+	// Popula os jogadores nas partidas
+	matchs := make([]Match, 0)
 
-	err = ga.Minimize(data.GenomeFactory)
+	for _, match := range body.Matchs {
+		player1, _ := FindPlayerByID(match.Player1, body.Players)
+		player2, _ := FindPlayerByID(match.Player2, body.Players)
+
+		matchs = append(matchs, Match{ID: match.ID, Player1: player1, Player2: player2})
+	}
+
+	scheduleData := ScheduleData{Slots: body.Slots, Players: body.Players, Matchs: matchs}
+
+	// Inicia o algorítmo genético
+
+	err = ga.Minimize(scheduleData.GenomeFactory)
+
 	if err != nil {
 		fmt.Println(err)
-		return
+		return ResponseBody{}, fmt.Errorf("erro")
 	}
 
-	fmt.Printf("Melhor solução %2.2f (%d/%d) de acertos\n", (1.0-ga.HallOfFame[0].Fitness)*100, int((1.0-ga.HallOfFame[0].Fitness)*float64(len(body.Matchs))), len(body.Matchs))
-	fmt.Println("Tempo total (segundos): ", time.Now().Sub(first).Seconds())
+	return ga.HallOfFame[0].Genome.(Schedule).BuildResponseFromSchedule(), nil
 
-	slots := ga.HallOfFame[0].Genome.(Schedule).Genome
+}
 
-	for slotIdx := 0; slotIdx < len(slots); slotIdx++ {
-		slot := data.Slots[slotIdx]
-		matchID := slots[slotIdx]
+// GenerateAllocation Procura uma boa alocação das partidas
+func GenerateAllocation(w http.ResponseWriter, r *http.Request) {
 
-		if matchID != -1 {
+	// timer := time.Now()
 
-			fmt.Println(matchID, slot.ID)
+	var body RequestBody
+	_ = json.NewDecoder(r.Body).Decode(&body)
 
-			for _, match := range data.Matchs {
+	response, _ := RunGeneticAlgorithm(body)
 
-				if match.ID == matchID {
+	json.NewEncoder(w).Encode(response)
 
-					player1, _ := FindPlayerByID(match.Player1, data.Players)
-					player2, _ := FindPlayerByID(match.Player2, data.Players)
+	// var body AllocateRequestBody
+	// _ = json.NewDecoder(r.Body).Decode(&body)
 
-					if !Contains(player1.AbleSlots, slot.ID) || !Contains(player2.AbleSlots, slot.ID) {
-						fmt.Println("Erro aqui")
-					}
+	// data := Schedule{Slots: body.Slots, Matchs: body.Matchs, Players: body.Players}
 
-					break
-				}
-			}
+	// // ga.Callback = func(ga *eaopt.GA) {
+	// // 	// fmt.Printf("Best fitness at generation %d: %f\n", ga.Generations, ga.HallOfFame[0].Fitness)
+	// // }
 
-		}
-	}
+	// fmt.Printf("Melhor solução: %f\n", ga.HallOfFame[0].Fitness)
+	// // fmt.Printf("Melhor solução %2.2f (%d/%d) de acertos\n", (1.0-ga.HallOfFame[0].Fitness)*100, int((1.0-ga.HallOfFame[0].Fitness)*float64(len(body.Matchs))), len(body.Matchs))
+	// fmt.Println("Tempo total (segundos): ", time.Now().Sub(first).Seconds())
+
+	// slots := ga.HallOfFame[0].Genome.(Schedule).Genome
+
+	// for slotIdx := 0; slotIdx < len(slots); slotIdx++ {
+	// 	slot := data.Slots[slotIdx]
+	// 	matchID := slots[slotIdx]
+
+	// 	if matchID != -1 {
+
+	// 		fmt.Println(matchID, slot.ID)
+
+	// 		for _, match := range data.Matchs {
+
+	// 			if match.ID == matchID {
+
+	// 				player1, _ := FindPlayerByID(match.Player1, data.Players)
+	// 				player2, _ := FindPlayerByID(match.Player2, data.Players)
+
+	// 				if !Contains(player1.AbleSlots, slot.ID) || !Contains(player2.AbleSlots, slot.ID) {
+	// 					fmt.Println("Erro aqui")
+	// 				}
+
+	// 				break
+	// 			}
+	// 		}
+
+	// 	}
+	// }
 
 	// for index := 0; index < len(slots); index++ {
 
@@ -279,8 +178,18 @@ func GenerateAllocation(w http.ResponseWriter, r *http.Request) {
 
 	// }
 
-	// fmt.Println(x)
-	json.NewEncoder(w).Encode(AllocateRequestResponse{Fitness: (1.0 - ga.HallOfFame[0].Fitness), Slots: slots})
+	// tmpSlots := make([]Slot, len(data.Slots))
+	// copy(tmpSlots, data.Slots)
+
+	// response := AllocateRequestResponse{Slots: tmpSlots}
+
+	// for index := 0; index < len(data.Slots); index++ {
+	// 	match, _ := FindMatchByID(slots[index], data.Matchs)
+
+	// 	response.Slots[index].Match = match
+	// }
+
+	// json.NewEncoder(w).Encode(response)
 }
 
 func main() {
